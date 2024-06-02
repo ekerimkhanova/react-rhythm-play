d8d76f978b8e23d73c76ce7c8ca12f743f1c2233
Emma Kerimkhanova <kerimkhanova93@yandex.ru>
2024-06-02 11:45:46 +0300

    Update screenshots

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import { AudioWave, useAudio } from "./index";
+import { AudioWave } from "./index";
 
 import audio from "./assets/audio/audio.ogg";
-import { useRef } from "react";
 
 function App() {
-
-  const audioWaveRef = useRef();
-
-  const { togglePlayPause } = useAudio(audioWaveRef);
-
-  return (
-    <div>
-      <AudioWave
-        ref={audioWaveRef}
-        height={400}
-        width={750}
-        audioSource={audio}
-        isCustomAudioBar
-      />
-      <button onClick={togglePlayPause}>play/pause</button>
-    </div>
-  );
+  return <AudioWave width={750} height={400} audioSource={audio} />;
 }
 
 export default App;
